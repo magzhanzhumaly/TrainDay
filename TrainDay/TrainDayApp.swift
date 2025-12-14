aeb6b0f9198b744788fdfcc99f3c577c249381dc
magzhanzhumaly <magzhanzhumaly@gmail.com>
2025-12-14 20:15:04 +0200

    Initial Commit

---
+++ TrainDay/TrainDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainDayApp.swift
+//  TrainDay
+//
+//  Created by Magzhan Zhumaly on 14.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
